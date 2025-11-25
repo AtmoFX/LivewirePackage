4381ab4ff15ef2b4de5e752b7a884179a18e4101
FX David <fxavierdavid@gmail.com>
2025-11-25 11:45:16 +0100

    Initial commit

---
+++ PackageServiceProvider.php
@@ -0,0 +1,40 @@
+<?php
+ 
+namespace AllYouNeed\AdvancedControls;
+ 
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\ServiceProvider;
+use Livewire\Livewire;
+
+use AllYouNeed\AdvancedControls\Views\Components\Button;
+ 
+final class PackageServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+        //     $this->commands(
+        //         commands: [
+        //             DataTransferObjectMakeCommand::class,
+        //         ],
+        //     );
+        }
+        else {
+            $this->registerComponents();
+        }   
+    }
+    public function registerComponents()
+    {
+        // Blade components work
+        Blade::component($prefix . 'button', Button::class);
+
+        // Livewire components don't
+
+        // If you uncomment the following line, the error becomes 
+        //Blade::anonymousComponentPath(__DIR__.'/Views/Components');
+        Livewire::addComponent(
+            name: 'counter',
+            viewPath: __DIR__.'Views/Components/counter.blade.php'
+        );
+    }
+}
---
+++ src/Views/Components/Button.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AtmoFX\Views\Components;
+
+use Illuminate\View\Component;
+
+class ThemeToggle extends Component
+{
+    public string $type       = 'classic';
+    public string $lightTheme = 'light';
+    public string $darkTheme  = 'dark'; 
+
+    public function __construct(?string $type)
+    {
+        if (isset($type))
+            $this->type = $type;
+    }
+
+    public function render(): View|Closure|string
+    {
+        return <<<'HTML'
+        <button class="bg-blue-400 text-white p-2 rounded" 
+            x-data
+            @click="alert('Button clicked!')"
+        >
+            {{ $slot }
+        </button>}
+        HTML;
+    }
+}
